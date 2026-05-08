84bd1a03f4476347d21bde5ccfd33f8d684306f1
g3780076-web <g3780076@gmail.com>
2026-05-08 13:04:26 +0200

    Commit 6 --> He añadido un archivo.js y he mejorado el css y el html de las 2 paginas.html

---
+++ javaScript/index.js
@@ -0,0 +1,5 @@
+const italic = document.querySelector(".as");
+italic.addEventListener("click", function(){
+    const a = document.querySelector("#datospersonales");
+    a.style.fontStyle = "italic";
+});
