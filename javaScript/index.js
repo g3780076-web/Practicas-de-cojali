e8e9ddc7816a39850d5ad16819fbec4b75f5ffe2
g3780076-web <g3780076@gmail.com>
2026-05-11 12:23:24 +0200

    Commit 7 --> Hemos puesto el modo oscuro y el modo claro con javaScript

--- javaScript/index.js
+++ javaScript/index.js
@@ -3,3 +3,29 @@ italic.addEventListener("click", function(){
     const a = document.querySelector("#datospersonales");
     a.style.fontStyle = "italic";
 });
+
+const a = document.body;
+    if (localStorage.getItem("Modo") === "oscuro") {
+    a.style.backgroundColor="rgb(56, 51, 51)";
+    a.style.color="white";
+    }else{
+        a.style.backgroundColor="white";
+        a.style.color="black";
+    }
+
+let count = 0;
+const oscuro = document.querySelector(".oscuro");
+oscuro.addEventListener("click",function(){
+    if (localStorage.getItem("Modo") === "oscuro") {
+
+        localStorage.setItem("Modo", "claro");
+        a.style.backgroundColor = "white";
+        a.style.color = "black";
+
+    } else {
+
+        localStorage.setItem("Modo", "oscuro");
+        a.style.backgroundColor = "rgb(54, 51, 51)";
+        a.style.color = "white";
+    }
+});
