b8a2cc519867f9f52084757a8d9d2a9a8e3c2312
g3780076-web <g3780076@gmail.com>
2026-05-14 13:03:24 +0200

    Commit 9 --> Hemos añadido lo de github en js

--- javaScript/index.js
+++ javaScript/index.js
@@ -45,3 +45,42 @@ function mostrarTexto(){
 
 
 
+ async function buscarUsuario() {
+      const username = document.getElementById("username").value.trim();
+      const card = document.getElementById("card");
+      const error = document.getElementById("error");
+
+      card.style.display = "none";
+      error.style.display = "none";
+
+      if (!username) {
+        error.textContent = "⚠️ Debes escribir un usuario";
+        error.style.display = "block";
+        return;
+      }
+
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
+        if (!response.ok) {
+          throw new Error("Usuario no encontrado");
+        }
+
+        const data = await response.json();
+        document.getElementById("name").textContent = data.name || data.login;
+        document.getElementById("bio").textContent = data.bio || "Sin bio disponible";
+        const lista = document.getElementById("repos");
+lista.innerHTML = "";
+
+data.forEach(repo => {
+  lista.innerHTML += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
+});
+
+
+        card.style.display = "block";
+
+      } catch (err) {
+        error.textContent = "❌ " + err.message;
+        error.style.display = "block";
+      }
+    }
